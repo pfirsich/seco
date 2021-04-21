e1cf11cb997d929a74a7af658c36bafa1d0b179b
Joel Schumacher <joelschum@gmail.com>
2021-04-21 20:21:33 +0200

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,12 @@ const auto usage = R"(seco test
 
 Usage:
     server-control start [--id=<id>]
-    server-control control [--id=<id>] show-id
-    server-control control [--id=<id>] exit
     server-control control [--id=<id>] status
     server-control control [--id=<id>] get-var <name>
     server-control control [--id=<id>] set-var <name> <value>
     server-control control [--id=<id>] print <message>
     server-control control [--id=<id>] long
+    server-control control [--id=<id>] exit
 
 Options:
     --id=<id>       Server instance ID
@@ -36,45 +35,47 @@ Options:
 char executeControlCommand(const std::vector<std::string>& command, seco::CommandOutput& output)
 {
     static std::unordered_map<std::string, std::string> variables;
+
+    std::map<std::string, docopt::value> args;
     try {
-        const auto args = docopt::docopt_parse(usage, command);
-        if (!args.at("control").asBool()) {
+        args = docopt::docopt_parse(usage, command);
+    } catch (const std::runtime_error& exc) {
+        return 1;
+    }
+
+    if (!args.at("control").asBool()) {
+        return 1;
+    }
+
+    if (args.at("status").asBool()) {
+        output.write("Everything seems fine\n");
+        return 0;
+    } else if (args.at("get-var").asBool()) {
+        const auto it = variables.find(args.at("<name>").asString());
+        if (it == variables.end()) {
+            output.write("Variable not found\n");
             return 1;
         }
-        if (args.at("show-id").asBool()) {
-            output.write("TODO\n");
-            return 0;
-        } else if (args.at("exit").asBool()) {
-            output.close(0);
-            std::exit(0);
-        } else if (args.at("status").asBool()) {
-            output.write("Everything seems fine\n");
-            return 0;
-        } else if (args.at("get-var").asBool()) {
-            const auto it = variables.find(args.at("<name>").asString());
-            if (it == variables.end()) {
-                output.write("Variable not found\n");
-                return 1;
-            }
-            output.write(it->second + "\n");
-            return 0;
-        } else if (args.at("set-var").asBool()) {
-            variables[args.at("<name>").asString()] = args.at("<value>").asString();
-            return 0;
-        } else if (args.at("print").asBool()) {
-            std::cout << args.at("<message>").asString() << std::endl;
-            return 0;
-        } else if (args.at("long").asBool()) {
-            for (size_t i = 0; i < 5; ++i) {
-                output.write("Processing..\n");
-                sleep(1);
-            }
-            output.write("Done.\n");
-            return 0;
+        output.write(it->second + "\n");
+        return 0;
+    } else if (args.at("set-var").asBool()) {
+        variables[args.at("<name>").asString()] = args.at("<value>").asString();
+        return 0;
+    } else if (args.at("print").asBool()) {
+        std::cout << args.at("<message>").asString() << std::endl;
+        return 0;
+    } else if (args.at("long").asBool()) {
+        for (size_t i = 0; i < 5; ++i) {
+            output.write("Processing..\n");
+            sleep(1);
         }
+        output.write("Done.\n");
+        return 0;
+    } else if (args.at("exit").asBool()) {
+        output.close(0);
+        std::exit(0);
+    } else {
         output.write("Unknown command\n");
-        return 1;
-    } catch (const std::runtime_error& exc) {
         return 1;
     }
 }
@@ -102,8 +103,8 @@ int main(int argc, char** argv)
 
         ::signal(SIGINT, signalHandler);
 
-        // You probably want to do this in your application, because SIGPIPE will be received if the
-        // control process was terminated while the server is still writing.
+        // You probably want to do this in your application, because SIGPIPE will be received if
+        // the control process was terminated while the server is still writing.
         ::signal(SIGPIPE, SIG_IGN);
 
         while (running.load()) {
